05558636ab3589c2981a843c6a6ccfe975c19aac
Jasmine Dang <ngandth.1992@gmail.com>
2020-08-08 23:08:03 +0800

    create a form in App

---
+++ src/components/app.jsx
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <form action='#'>
+          <input type='text' />
+          <input type='submit' />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default App;
