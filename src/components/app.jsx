4edd37a14c901ff42a281c40aa5ba277a0fd1601
Jasmine Dang <ngandth.1992@gmail.com>
2020-08-08 23:12:19 +0800

    add empty handleSubmit

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,10 +1,14 @@
 import React, {Component} from 'react';
 
 class App extends Component {
+  handleSubmit = () => {
+    console.log('hi');
+  };
+
   render() {
     return (
       <div>
-        <form action='#'>
+        <form action='#' onSubmit={this.handleSubmit}>
           <input type='text' />
           <input type='submit' />
         </form>
