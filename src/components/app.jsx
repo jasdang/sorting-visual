199dc32b8decf6e6c267050187c67aa81c21e60d
Jasmine Dang <ngandth.1992@gmail.com>
2020-08-09 22:32:20 +0800

    added sort button with handleClick event

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import Input from './input';
 import TileList from './tile_list';
+import quickSort from './sorting_algorithms';
 class App extends Component {
   constructor(props) {
     super(props);
@@ -13,11 +14,16 @@ class App extends Component {
     console.log(this.state);
   };
 
+  handleClick = () => {
+    this.addTiles([4, 5, 6]);
+  };
+
   render() {
     return (
       <div>
         <Input updateTiles={this.addTiles} />
         <TileList values={this.state.tiles} />
+        <button onClick={this.handleClick}>{'Sort'}</button>
       </div>
     );
   }
