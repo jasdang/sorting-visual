a418dbd2dd86711ec1ebb8488f2ef8b12628520c
Jasmine Dang <ngandth.1992@gmail.com>
2020-08-11 22:43:41 +0800

    make quickSort async and update graph every swap

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -2,7 +2,8 @@ import React, {Component} from 'react';
 import Input from './input';
 import TileList from './tile_list';
 import Bar from './bar';
-import quickSort from './sorting_algorithms';
+import regeneratorRuntime from 'regenerator-runtime';
+// import quickSort from './sorting_algorithms';
 class App extends Component {
   constructor(props) {
     super(props);
@@ -15,26 +16,87 @@ class App extends Component {
   };
   generateArray = () => {
     let array = [];
-    for (let i = 0; i < 20; i++) {
+    for (let i = 0; i < 400; i++) {
       array.push(Math.floor(Math.random() * 100));
     }
     this.addTiles(array);
-    console.log(array);
   };
 
   handleClick = () => {
-    this.addTiles(quickSort(this.state.tiles));
+    this.quickSort(this.state.tiles);
   };
 
   render() {
     return (
-      <div className='container'>
+      <div>
         {/* <Input updateTiles={this.addTiles} /> */}
         <TileList values={this.state.tiles} />
         <button onClick={this.generateArray}>{'Generate'}</button>
         <button onClick={this.handleClick}>{'Sort'}</button>
       </div>
     );
+  }
+
+  // TESTING
+  quickSort = (array) => {
+    return this.quickSortHelper(array, 0, array.length - 1);
+  };
+
+  async quickSortHelper(array, startId, endId) {
+    let t = 1;
+    if (endId - startId < 1) {
+      this.addTiles(array);
+      return array;
+    }
+
+    if (endId - startId === 1) {
+      if (array[endId] < array[startId]) {
+        await this.swap(array, startId, endId);
+      }
+      this.addTiles(array);
+      return array;
+    }
+
+    let pointer = array[startId];
+    let i = startId + 1;
+    let j = endId;
+    while (i <= j) {
+      if (array[i] > pointer && array[j] <= pointer) {
+        await this.swap(array, i, j);
+      } else if (array[i] <= pointer) {
+        i++;
+      } else if (array[j] > pointer) {
+        j--;
+      }
+      this.addTiles(array);
+    }
+    await this.swap(array, startId, j);
+    this.addTiles(array);
+    if (j - startId < endId - j) {
+      await Promise.all([
+        this.quickSortHelper(array, startId, j - 1),
+        this.quickSortHelper(array, j + 1, endId),
+      ]);
+    } else {
+      await Promise.all([
+        this.quickSortHelper(array, j + 1, endId),
+        this.quickSortHelper(array, startId, j - 1),
+      ]);
+    }
+    this.addTiles(array);
+    return array;
+  }
+
+  async swap(array, i, j) {
+    await this.sleep(25);
+    let temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+    return array;
+  }
+
+  sleep(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
   }
 }
 
--- src/components/bar.jsx
+++ src/components/bar.jsx
@@ -4,10 +4,10 @@ const Bar = (props) => {
   const styleBar = {
     display: 'inline-block',
     // border: '1px solid black',
-    width: '10px',
+    width: '2px',
     height: `${props.height}px`,
     backgroundColor: 'rgb(50, 115, 220)',
-    margin: '1px',
+    margin: '0px',
   };
   return <div style={styleBar}></div>;
 };
