8f50f3990b0d5e60e136fc7fdf1b1ad842f61070
Jasmine Dang <ngandth.1992@gmail.com>
2020-08-29 23:51:54 +0800

    added redux and redux store

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+import {configuredStore} from '../store';
 import App from './components/app';
 import '../assets/stylesheets/application.scss';
 
 const root = document.getElementById('root');
 if (root) {
-  ReactDOM.render(<App />, root);
+  ReactDOM.render(
+    <Provider store={configuredStore()}>
+      <App />
+    </Provider>,
+    root
+  );
 }
