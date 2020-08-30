51315224db097d32dab35ba86aa2146deae4cb02
Jasmine Dang <ngandth.1992@gmail.com>
2020-08-30 22:23:27 +0800

    added action to generate array

---
+++ src/components/actions.js
@@ -0,0 +1,4 @@
+export const GENERATE_ARRAY = 'GENERATE_ARRAY';
+export const generateArray = () => ({
+  type: GENERATE_ARRAY,
+});
---
+++ src/components/reducers.js
@@ -0,0 +1,15 @@
+import {generateArray} from './actions';
+
+export const array = (state, action) => {
+  const {type, payload} = action;
+
+  switch (type) {
+    case GENERATE_ARRAY: {
+      let array = [];
+      for (let i = 0; i < 100; i++) {
+        array.push(Math.floor(Math.random() * 100));
+      }
+      return;
+    }
+  }
+};
