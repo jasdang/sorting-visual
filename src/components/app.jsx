c0454aa116dd520bfb8aadc95f9e03092dc2c8ad
Jasmine Dang <ngandth.1992@gmail.com>
2020-08-31 23:39:11 +0800

    convert App to function component

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -4,58 +4,31 @@ import {generateArray} from './actions';
 import TileList from './tile_list';
 import Bar from './bar';
 import regeneratorRuntime from 'regenerator-runtime';
-// import quickSort from './sorting_algorithms';
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      tiles: [],
-      colors: [],
-    };
-  }
-  addTiles = (array) => {
-    this.setState({tiles: array});
-  };
-  addColors = (array) => {
-    this.setState({colors: array});
-  };
-  // generateArray = () => {
-  //   let array = [];
-  //   for (let i = 0; i < 100; i++) {
-  //     array.push(Math.floor(Math.random() * 100));
-  //   }
-  //   this.addTiles(array);
-  // };
 
-  handleClick = () => {
-    this.quickSort(this.state.tiles);
+const App = ({tiles = [], colors = [], onGenerateArrayPressed}) => {
+  const addTiles = (array) => {
+    return array;
+  };
+  const addColors = (array) => {
+    return array;
   };
 
-  render() {
-    return (
-      <div>
-        {/* <Input updateTiles={this.addTiles} /> */}
-        <TileList values={this.state.tiles} colors={this.state.colors} />
-        <button onClick={this.props.onGenerateArrayPressed}>
-          {'Generate'}
-        </button>
-        <button onClick={this.handleClick}>{'Sort'}</button>
-      </div>
-    );
-  }
+  const handleClick = () => {
+    quickSort(tiles);
+  };
 
   // TESTING
-  quickSort = (array) => {
+  const quickSort = (array) => {
     let colors = Array(array.length).fill(false);
-    return this.quickSortHelper(array, 0, array.length - 1, colors);
+    return quickSortHelper(array, 0, array.length - 1, colors);
   };
 
-  async quickSortHelper(array, startId, endId, colors) {
+  const quickSortHelper = async (array, startId, endId, colors) => {
     if (endId - startId < 1) {
       colors[endId] = true;
       colors[startId] = true;
-      this.addColors(colors);
-      this.addTiles(array);
+      addColors(colors);
+      addTiles(array);
       colors[endId] = false;
       colors[startId] = false;
       return array;
@@ -63,12 +36,12 @@ class App extends Component {
 
     if (endId - startId === 1) {
       if (array[endId] < array[startId]) {
-        await this.swap(array, startId, endId);
+        await swap(array, startId, endId);
       }
       colors[endId] = true;
       colors[startId] = true;
-      this.addColors(colors);
-      this.addTiles(array);
+      addColors(colors);
+      addTiles(array);
       colors[endId] = false;
       colors[startId] = false;
       return array;
@@ -80,61 +53,71 @@ class App extends Component {
     while (i <= j) {
       colors[startId] = true;
       if (array[i] > pointer && array[j] <= pointer) {
-        await this.swap(array, i, j);
+        await swap(array, i, j);
       } else if (array[i] <= pointer) {
         colors[i] = false;
         i++;
         colors[i] = true;
-        this.addColors(colors);
+        addColors(colors);
       } else if (array[j] > pointer) {
         colors[j] = false;
         j--;
         colors[j] = true;
-        this.addColors(colors);
+        addColors(colors);
       }
       // this.addColors(colors);
-      this.addTiles(array);
+      addTiles(array);
     }
     colors[startId] = false;
     colors[i] = false;
     colors[j] = false;
-    this.addColors(colors);
-    await this.swap(array, startId, j);
+    addColors(colors);
+    await swap(array, startId, j);
     colors[startId] = true;
     colors[j] = true;
-    this.addColors(colors);
-    this.addTiles(array);
+    addColors(colors);
+    addTiles(array);
     colors[startId] = false;
     colors[j] = false;
     if (j - startId < endId - j) {
       await Promise.all([
-        this.quickSortHelper(array, startId, j - 1, colors),
-        this.quickSortHelper(array, j + 1, endId, colors),
+        quickSortHelper(array, startId, j - 1, colors),
+        quickSortHelper(array, j + 1, endId, colors),
       ]);
     } else {
       await Promise.all([
-        this.quickSortHelper(array, j + 1, endId, colors),
-        this.quickSortHelper(array, startId, j - 1, colors),
+        quickSortHelper(array, j + 1, endId, colors),
+        quickSortHelper(array, startId, j - 1, colors),
       ]);
     }
-    this.addTiles(array);
+    addTiles(array);
     return array;
-  }
+  };
 
-  async swap(array, i, j) {
-    await this.sleep(0);
+  const swap = async (array, i, j) => {
+    await sleep(0);
     let temp = array[i];
     array[i] = array[j];
     array[j] = temp;
     return array;
-  }
+  };
 
-  sleep(ms) {
+  const sleep = (ms) => {
     return new Promise((resolve) => setTimeout(resolve, ms));
-  }
-}
+  };
+
+  return (
+    <div>
+      {/* <Input updateTiles={this.addTiles} /> */}
+      <TileList values={tiles} colors={colors} />
+      <button onClick={onGenerateArrayPressed}>{'Generate'}</button>
+      <button onClick={handleClick}>{'Sort'}</button>
+    </div>
+  );
+};
 const mapStateToProps = (state) => ({
-  array: state.array,
+  array: state.tiles,
+  color: [],
 });
 const mapDispatchToProps = (dispatch) => ({
   onGenerateArrayPressed: () => {
--- src/components/reducers.js
+++ src/components/reducers.js
@@ -11,7 +11,7 @@ export const array = (state = [], action) => {
       }
       return {
         ...state,
-        array: array,
+        tiles: array,
       };
     }
     default:
