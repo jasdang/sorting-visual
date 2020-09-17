06c4e5cfb4d8528ad40ccff96792c0091c447f42
Jasmine Dang <ngandth.1992@gmail.com>
2020-09-17 22:38:14 +0800

    fix favicon in production

--- src/index.jsx
+++ src/index.jsx
@@ -4,6 +4,7 @@ import {Provider} from 'react-redux';
 import {store} from '../store';
 import App from './components/app';
 import '../assets/stylesheets/application.scss';
+require('../assets/favicon.ico');
 
 const root = document.getElementById('root');
 if (root) {
--- webpack.config.js
+++ webpack.config.js
@@ -4,34 +4,39 @@ const path = require('path');
 module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: 'bundle.js',
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: '!!html-loader!templates/index.html'
-    })
+      template: '!!html-loader!templates/index.html',
+      favicon: './assets/favicon.ico',
+    }),
   ],
   devtool: 'sourcemap',
-  mode: "development",
+  mode: 'development',
   module: {
     rules: [
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
-        loader: 'babel-loader'
+        loader: 'babel-loader',
       },
       {
         test: /\.s?css$/,
         exclude: /node_modules/,
-        loaders: [ 'style-loader', 'css-loader', 'sass-loader' ]
+        loaders: ['style-loader', 'css-loader', 'sass-loader'],
       },
       {
         test: /\.html$/,
-        loader: 'html-loader'
+        loader: 'html-loader',
       },
-    ]
+      {
+        test: /\.jpe?g$|\.ico$|\.gif$|\.png$|\.svg$|\.woff$|\.ttf$|\.wav$|\.mp3$/,
+        loader: 'file-loader?name=[name].[ext]', // <-- retain original file name
+      },
+    ],
   },
   resolve: {
-    extensions: [ '.js', '.jsx' ]
-  }
+    extensions: ['.js', '.jsx'],
+  },
 };
